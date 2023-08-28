1da8f70b94d977f3af92e3ebe60ec271da7f8e0c
DBalino <141344097+DBalino@users.noreply.github.com>
2023-08-28 15:40:07 +0800

    Updated 08.28.23

--- App.js
+++ App.js
@@ -1,205 +1,44 @@
-import * as React from 'react';
-import { View, Text, Image, TouchableOpacity, Button } from 'react-native';
-
-import {
-  createDrawerNavigator,
-  DrawerContentScrollView,
-  DrawerItemList,
-} from '@react-navigation/drawer';
+APP.JS
+import React, { useState } from "react";  
+import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-<<<<<<< HEAD
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Login from './screens/Login';
 import Registration from './screens/Registration';
 import LoadingScreen from './screens/LoadingScreen';
 import Homepage from './screens/BottomTab/Homepage';
-import BottomTabNavigator from "./screens/BottomTabNavigator";
-=======
->>>>>>> 94a9112c8c33e398744286155c41724801e7e98e
+import BottomTabNavigator from './screens/BottomTabNavigator';
+import Sidebar from './screens/Sidebar';
+import { AuthProvider } from './screens/AuthProvider';
 
-function Home({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home</Text>
-      {/* <Button onPress={() => navigation.toggleDrawer()} title='test' /> */}
-    </View>
-  );
-}
-
-function Tasks() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Tasks</Text>
-    </View>
-  );
-}
-
+const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
-const CustomDrawer = props => {
-  return (
-    <View style={{ flex: 1 }}>
-      <DrawerContentScrollView {...props}>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            padding: 20,
-            backgroundColor: '#f6f6f6',
-            marginBottom: 20,
-          }}
-        >
-          <View>
-            <Text>John Doe</Text>
-            <Text>example@email.com</Text>
-          </View>
-          <Image
-            source={{
-              uri: 'https://images.unsplash.com/photo-1624243225303-261cc3cd2fbc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80',
-            }}
-            style={{ width: 60, height: 60, borderRadius: 30 }}
-          />
-        </View>
-        <DrawerItemList {...props} />
-      </DrawerContentScrollView>
-      <TouchableOpacity
-        style={{
-          position: 'absolute',
-          right: 0,
-          left: 0,
-          bottom: 50,
-          backgroundColor: '#f6f6f6',
-          padding: 20,
-        }}
-      >
-        <Text>Log Out</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
+const MainStack = () => (
+  <Stack.Navigator>
+       <Stack.Screen options={{headerShown: false}} name="Loading Screen" component={LoadingScreen} />
+        <Stack.Screen options={{headerShown: false}} name="Login Page" component={Login} />
+        <Stack.Screen options={{headerShown: false}} name="Registration Page" component={Registration} />
+        <Stack.Screen options={{headerShown: false}} name="Home Page" component={Homepage} />
+        <Stack.Screen options={{headerShown: false}} name="Bottom Bar" component={BottomTabNavigator} />
+  </Stack.Navigator>
+);
 
-const DrawerNavigator = () => {
-  return (
-    <Drawer.Navigator
-      screenOptions={{
-        headerShown: true,
-        headerStyle: {
-          backgroundColor: 'transparent',
-          elevation: 0,
-          shadowOpacity: 0,
-        },
-        headerTitle: '',
-      }}
-      drawerContent={props => <CustomDrawer {...props} />}
-    >
-      <Drawer.Screen component={Home} name='Home' />
-      <Drawer.Screen component={Tasks} name='Tasks' />
-    </Drawer.Navigator>
-  );
-};
-
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
-      <DrawerNavigator />
+      <AuthProvider>
+        <Drawer.Navigator initialRouteName="MainStack" drawerContent={(props) => <Sidebar {...props} CustomDrawerContent{...props} />}>
+          <Drawer.Screen name="Mainstack" component={MainStack} />
+          {/* Add other drawers */}
+        </Drawer.Navigator>
+      </AuthProvider>
     </NavigationContainer>
-  );
-}import * as React from 'react';
-import { View, Text, Image, TouchableOpacity, Button } from 'react-native';
 
-import {
-  createDrawerNavigator,
-  DrawerContentScrollView,
-  DrawerItemList,
-} from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-
-function Home({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home</Text>
-      {/* <Button onPress={() => navigation.toggleDrawer()} title='test' /> */}
-    </View>
   );
 }
 
-function Tasks() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Tasks</Text>
-    </View>
-  );
-}
+export default App; 	
 
-const Drawer = createDrawerNavigator();
 
-const CustomDrawer = props => {
-  return (
-    <View style={{ flex: 1 }}>
-      <DrawerContentScrollView {...props}>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            padding: 20,
-            backgroundColor: '#f6f6f6',
-            marginBottom: 20,
-          }}
-        >
-          <View>
-            <Text>John Doe</Text>
-            <Text>example@email.com</Text>
-          </View>
-          <Image
-            source={{
-              uri: 'https://images.unsplash.com/photo-1624243225303-261cc3cd2fbc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80',
-            }}
-            style={{ width: 60, height: 60, borderRadius: 30 }}
-          />
-        </View>
-        <DrawerItemList {...props} />
-      </DrawerContentScrollView>
-      <TouchableOpacity
-        style={{
-          position: 'absolute',
-          right: 0,
-          left: 0,
-          bottom: 50,
-          backgroundColor: '#f6f6f6',
-          padding: 20,
-        }}
-      >
-        <Text>Log Out</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
 
-const DrawerNavigator = () => {
-  return (
-    <Drawer.Navigator
-      screenOptions={{
-        headerShown: true,
-        headerStyle: {
-          backgroundColor: 'transparent',
-          elevation: 0,
-          shadowOpacity: 0,
-        },
-        headerTitle: '',
-      }}
-      drawerContent={props => <CustomDrawer {...props} />}
-    >
-      <Drawer.Screen component={Home} name='Home' />
-      <Drawer.Screen component={Tasks} name='Tasks' />
-    </Drawer.Navigator>
-  );
-};
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <DrawerNavigator />
-    </NavigationContainer>
-  );
-};
